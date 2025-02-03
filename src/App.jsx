5c4dad6043d7d490d91c6d4136b8e082d18e8f04
Vinay-githubb <vinay.ky.07@gmail.com>
2025-02-02 23:48:27 -0500

    Learned about states in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
       <Navbar title="TextUtil" />
+
+      <div className="container">
+        <TextForm heading="Enter the text here" />
+      </div>
     </>
   );
 }
---
+++ src/components/TextForm.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    console.log("Upper case was clicked " + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const handleOnChange = (event) => {
+    console.log("On change");
+    setText(event.target.value);
+  };
+
+  const [text, setText] = useState("Enter  text here2 ");
+  return (
+    <div>
+      <h1>{props.heading}</h1>
+      <div className="mb-3">
+        <textarea
+          className="form-control"
+          value={text}
+          onChange={handleOnChange}
+          id="myBox"
+          rows="8"
+        ></textarea>
+      </div>
+      <button className="btn btn-primary" onClick={handleUpClick}>
+        Convert to upper case
+      </button>
+    </div>
+  );
+}
