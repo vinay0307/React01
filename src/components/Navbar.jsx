dd24a99a5598a3f7ecb2869d72e3ffae49f56518
Vinay-githubb <vinay.ky.07@gmail.com>
2025-02-03 00:35:43 -0500

    Added some new features

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ export default function Navbar(props) {
               placeholder="Search"
               aria-label="Search"
             />
-            <button className="btn btn-outline-success" type="submit">
+            <button className="btn btn-outline-primary" type="submit">
               Search
             </button>
           </form>
--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -6,28 +6,41 @@ export default function TextForm(props) {
     let newText = text.toUpperCase();
     setText(newText);
   };
+const handleLoClick = () => {
+  //console.log("Upper case was clicked " + text);
+  let newText = text.toLowerCase();
+  setText(newText);
+};
+
 
   const handleOnChange = (event) => {
     console.log("On change");
     setText(event.target.value);
   };
 
-  const [text, setText] = useState("Enter  text here2 ");
+  const [text, setText] = useState(" ");
   return (
-    <div>
+    <>
+    <div className= "container">
       <h1>{props.heading}</h1>
       <div className="mb-3">
-        <textarea
-          className="form-control"
+        <textarea className="form-control"
           value={text}
           onChange={handleOnChange}
           id="myBox"
           rows="8"
         ></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>
-        Convert to upper case
-      </button>
+      <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to upper case</button>
+      <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lower case</button>
     </div>
+    <div className= "container my-2">
+    <h1>You text summary</h1>
+    <p>{text.split(" ").length} words and {text.length} characters</p>
+     <p>{0.008 *text.split(" ").length }</p>
+     <h2>Preview</h2>
+     <p>{text}</p>
+    </div>
+    </>
   );
 }
